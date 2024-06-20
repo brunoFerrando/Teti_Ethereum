128c9f698301f25933846473b2efdcb86756beb0
Bruno Ferrando <bruno.ferrando@fing.edu.uy>
2024-06-19 21:09:29 -0300

    prueba

--- Smart_Test_Contract.sol
+++ Smart_Test_Contract.sol
@@ -3,12 +3,15 @@ pragma solidity 0.8.26;
 
 contract Billboard {
     string public message;
+    int changes;
     
     constructor(string memory _message) {
         message = _message;
+        changes = 0;
     }
     
     function writeBillboard(string memory _message) public {
         message = _message;
+        changes = changes + 1;
     }
 }
